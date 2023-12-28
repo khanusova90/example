a696ca1b20e27d66856ace248ee75530e6804daf
Kateřina Hanušová <k.hanusova90@gmail.com>
2023-12-28 18:20:25 +0100

    Initial commit

---
+++ src/test/kotlin/cz/hanusova/example/ExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package cz.hanusova.example
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
